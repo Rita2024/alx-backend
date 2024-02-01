d60f8acedd47c9998bc1f2826bd3f400e0ab64b3
Rita2024 <ritaejezie2@gmail.com>
2024-02-01 18:29:33 +0000

    fifo cache

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -3,7 +3,6 @@
 """Task 1: FIFO caching
 """
 
-
 from collections import OrderedDict
 from base_caching import BaseCaching
 
@@ -14,23 +13,30 @@ class FIFOCache(BaseCaching):
     """
 
     def __init__(self):
+        """Initialize FIFOCache instance"""
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """assign to the dictionary `self.cache_data` the
-        `item` value for the key `key`
+        """Assign to the dictionary `self.cache_data` the
+        `item` value for the key `key`.
+        If key or item is None, do nothing.
+        If the number of items in `self.cache_data` is higher than BaseCaching.MAX_ITEMS,
+        discard the first item put in the cache (FIFO algorithm).
+        Print DISCARD with the key discarded.
         """
 
         if key is None or item is None:
             return
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key, _ = self.cache_data.popitem(last=False)
-            print(f"DISCARD: {first_key}")
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discarded_key, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {discarded_key}")
 
         self.cache_data[key] = item
 
     def get(self, key):
-        """return the value in `self.cache_data` linked to `key`"""
+        """Return the value in `self.cache_data` linked to `key`.
+        If key is None or if the key doesn’t exist in `self.cache_data`, return None.
+        """
         return self.cache_data.get(key, None)
