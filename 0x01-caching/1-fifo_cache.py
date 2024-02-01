3fddc8e70a2c80193e45218e8e1e3f5b26978bb0
Rita2024 <ritaejezie2@gmail.com>
2024-02-01 18:52:38 +0000

    updated

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -3,7 +3,9 @@
 """Task 1: FIFO caching
 """
 
+from collections import OrderedDict
 from base_caching import BaseCaching
+
 
 class FIFOCache(BaseCaching):
     """A class `FIFOCache` that inherits from
@@ -13,6 +15,7 @@ class FIFOCache(BaseCaching):
     def __init__(self):
         """Initialize FIFOCache instance"""
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """Assign to the dictionary `self.cache_data` the
